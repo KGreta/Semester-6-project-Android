1c22d1dc9841ee94063adbfef658e45acdaa1410
gretakomaromi <greta.komaromi@gmail.com>
2022-05-16 20:50:06 +0200

    Initial

--- app/src/main/java/dk/via/wishlist/ui/my_wishlist/WishlistFragment.java
+++ app/src/main/java/dk/via/wishlist/ui/my_wishlist/WishlistFragment.java
@@ -4,6 +4,8 @@ import static android.content.ContentValues.TAG;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +13,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -96,11 +100,71 @@ public class WishlistFragment extends Fragment {
 
     }
 
+    private void GetWishesFromWishlist()
+    {
+        String username = PreferenceConfig.getUsername(getContext());
+        ListView listview = getView().findViewById(R.id.wish_list);
+        ArrayList<String> wishes = new ArrayList<>();
+
+        ArrayAdapter<String> dialogAdapter = new ArrayAdapter<String>(
+                getContext(),
+                android.R.layout.select_dialog_item,
+                wishes);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.create();
+        builder.setIcon(R.drawable.ic_wishlist);
+        builder.setAdapter(dialogAdapter, null);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String wishlist = (String) adapterView.getAdapter().getItem(i);
+                builder.setTitle("Wishes in " + wishlist);
+                myRef.child(username).child("wishlist").child(wishlist).getRef().addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        String value = dataSnapshot.getValue(String.class);
+                        if(!TextUtils.isEmpty(value)) {
+                            wishes.add(value);
+                            dialogAdapter.notifyDataSetChanged();
+                        }
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                builder.show();
+                wishes.clear();
+                dialogAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(WishlistViewModel.class);
         GetWishlists();
+        GetWishesFromWishlist();
         // TODO: Use the ViewModel
     }
 
